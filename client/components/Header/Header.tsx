c704510de1c3a561c13c73412a3833a35c52fcef
AbdallahShnaino <abdallah.shnaino@gmail.com>
2025-07-22 18:09:58 +0300

    some modularity in settings screen for unused components and code

--- client/components/Header/Header.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import Image from "next/image";
-import Profile from "../Profile/Profile";
-import SelectLang from "../SelectLanguage/SelectLanguage";
-import HeaderSearch from "../HeaderSearch/HeaderSearch";
-
-export default function Header() {
-  return <h1>a</h1>;
-}
